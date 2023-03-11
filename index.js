38a72f94b819d3a9a829772c5dfebf8f54759700
Aizen <iansky3311@gmail.com>
2023-03-11 09:38:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,61 @@
+function add(integer){
+    console.log(integer);
+}
+function subtract(integer){
+    console.log(integer)
+}
+function multiply(integer){
+    console.log(integer)
+}
+function divide(integer){
+    console.log(integer)
+}
+
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function add(a,b){
+    return a + b
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function subtract(a,b){
+    return a - b
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function multiply(a,b){
+    return a * b
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function divide(a,b){
+    return a / b
+}
+
+a = Math.floor(Math.random() * 1000)
+function increment(a){
+    return a+=1
+}
+a = Math.floor(Math.random() * 1000)
+function decrement(a){
+    return a-=1
+}
+
+a = parseInt(Math.floor(Math.random() * 1000))
+function makeInt(a){
+    return a
+}
+
+a = Math.floor(Math.random() * 1000)
+function makeInt(a){
+    return parseInt(a, 10)
+}
+
+a = Math.floor(Math.random() * 1000)
+function preserveDecimal(a){
+return parseFloat(a)
+}
